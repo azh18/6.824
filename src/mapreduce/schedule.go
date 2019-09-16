6cda6f116022cd7c9927bf6b4525d23c4b42ce79
zbw0046 <zbw0046@gmail.com>
2019-09-17 00:07:42 +0800

    finish lab1 stage 3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -24,5 +27,30 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	// read workers from channel
+	wg := sync.WaitGroup{}
+	for i := 0; i < ntasks; i++ {
+		doTaskArgs := DoTaskArgs{
+			JobName:       mr.jobName,
+			File:          mr.files[i],
+			Phase:         phase,
+			TaskNumber:    i,
+			NumOtherPhase: nios,
+		}
+		worker := <-mr.registerChannel
+		go dispatchTask(worker, &doTaskArgs, &wg, mr.registerChannel)
+		wg.Add(1)
+	}
+	wg.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
+}
+
+func dispatchTask(workerName string, doTaskArgs *DoTaskArgs, wg *sync.WaitGroup, registerChannel chan string) bool {
+	defer func() {
+		go func() {
+			registerChannel <- workerName
+		}()
+		wg.Done()
+	}()
+	return call(workerName, "Worker.DoTask", *doTaskArgs, nil)
 }
