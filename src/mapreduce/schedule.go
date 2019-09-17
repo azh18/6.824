5a9cbac3781c77af85d21d8b77d4a6b3edc565d4
zbw0046 <zbw0046@gmail.com>
2019-09-17 10:41:10 +0800

    finish and pass lab1

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -28,29 +28,55 @@ func (mr *Master) schedule(phase jobPhase) {
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
 	// read workers from channel
+
 	wg := sync.WaitGroup{}
+	taskChan := make(chan int)
+	go mr.TaskRunner(ntasks, taskChan, &wg, phase, nios)
 	for i := 0; i < ntasks; i++ {
-		doTaskArgs := DoTaskArgs{
-			JobName:       mr.jobName,
-			File:          mr.files[i],
-			Phase:         phase,
-			TaskNumber:    i,
-			NumOtherPhase: nios,
-		}
-		worker := <-mr.registerChannel
-		go dispatchTask(worker, &doTaskArgs, &wg, mr.registerChannel)
+		taskChan <- i
 		wg.Add(1)
 	}
 	wg.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
 
-func dispatchTask(workerName string, doTaskArgs *DoTaskArgs, wg *sync.WaitGroup, registerChannel chan string) bool {
-	defer func() {
+func (mr *Master) TaskRunner(nTasks int, taskChan chan int, wg *sync.WaitGroup, phase jobPhase, nios int) {
+	remainTasksCnt := nTasks
+	for remainTasksCnt > 0 {
+		select {
+		case taskId := <-taskChan:
+			doTaskArgs := DoTaskArgs{
+				JobName:       mr.jobName,
+				File:          mr.files[taskId],
+				Phase:         phase,
+				TaskNumber:    taskId,
+				NumOtherPhase: nios,
+			}
+			worker := <-mr.registerChannel
+			result := dispatchTask(worker, &doTaskArgs, wg, mr.registerChannel, taskChan, taskId)
+			if result {
+				remainTasksCnt--
+			}
+		default:
+			if remainTasksCnt <= 0 {
+				break
+			}
+		}
+	}
+}
+
+func dispatchTask(workerName string, doTaskArgs *DoTaskArgs, wg *sync.WaitGroup, registerChannel chan string, taskChan chan int, taskId int) bool {
+	result := call(workerName, "Worker.DoTask", *doTaskArgs, nil)
+	if result {
 		go func() {
 			registerChannel <- workerName
 		}()
 		wg.Done()
-	}()
-	return call(workerName, "Worker.DoTask", *doTaskArgs, nil)
+	} else {
+		fmt.Printf("Task %d failed. Retry.\n", taskId)
+		go func() {
+			taskChan <- taskId
+		}()
+	}
+	return result
 }
